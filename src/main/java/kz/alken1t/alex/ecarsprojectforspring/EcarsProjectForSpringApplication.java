06aa066f1ba6ed658f7e3e9d3fd1f648163f5729
Алексей Зарубин <alken1t15@gmail.com>
2023-06-02 23:44:59 +0600

    first commit

---
+++ src/main/java/kz/alken1t/alex/ecarsprojectforspring/EcarsProjectForSpringApplication.java
@@ -0,0 +1,13 @@
+package kz.alken1t.alex.ecarsprojectforspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcarsProjectForSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcarsProjectForSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/alken1t/alex/ecarsprojectforspring/EcarsProjectForSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.alken1t.alex.ecarsprojectforspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcarsProjectForSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
